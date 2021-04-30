988f904a2129d365db517aafcf465d0de4cd9b0d
Abhirup Das <58749843+rexetorize@users.noreply.github.com>
2021-04-30 20:05:40 +0530

    Update tailwind.config.js
    C of the component will be uppercase! Line 4.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   mode: 'jit',
   purge: [
+    './pages/**/*.{js,ts,jsx,tsx}', './Components/**/*.{js,ts,jsx,tsx}',
    ],
   darkMode: false, // or 'media' or 'class'
   theme: {
